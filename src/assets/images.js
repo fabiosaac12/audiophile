919a4a30fc4a83283c455b57f6fcb32450f815c4
Fabio Isaac <fabiosaac12@gmail.com>
2021-08-23 22:29:53 -0400

    feat: header

---
+++ src/assets/images.js
@@ -0,0 +1,4 @@
+import logo from './png/logo.png';
+import headphones1 from './png/headphones1.png';
+
+export const images = { logo, headphones1 };
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import { images } from '../../assets';
+import classes from './HeaderStyles.module.scss';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+
+export const Header = () => (
+  <div className={`container ${classes.container}`}>
+    <img src={images.logo} className={classes.logo} alt="logo" />
+    <div className={classes.navigation}>
+      <span className={classes.button}>Home</span>
+      <span className={classes.button}>Headphones</span>
+      <span className={classes.button}>Speakers</span>
+      <span className={classes.button}>Earphones</span>
+    </div>
+    <div className={`${classes.shoppingButton} ${classes.button}`}>
+      <AiOutlineShoppingCart className={classes.shoppingCarIcon} />
+    </div>
+  </div>
+);
